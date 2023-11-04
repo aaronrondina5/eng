8d6e35b8e96174091a78733e13bb7fdc9a625b6d
Aaron Rondina <aaronrondina5@gmail.com>
2023-11-04 18:59:18 -0400

    FIX HUGE ISSUE NO CODE NEED URGENT REVIEWWWW Initialize python app, unittests, problems 1,2 (#2)
    * Initialize python app, unittests, problems 1,2
    
    - complete problems 1 and 2
    - set up python app with unittest
    - set up github actions
    
    * use _test.py

---
+++ tests/eulerproblems/multiples_three_five_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from app.eulerproblems.multiples_three_five import MultiplesThreeFive
+
+
+class MultiplesThreeFiveTests(unittest.TestCase):
+    def test_end_on_shared_multiple(self):
+        algo = MultiplesThreeFive()
+        self.assertEqual(60, algo.brute_force(end_inclusive=15))
+        self.assertEqual(60, algo.optimal(end_inclusive=15))
+
+    def test_end_on_multiple_of_three(self):
+        algo = MultiplesThreeFive()
+        self.assertEqual(23, algo.brute_force(end_inclusive=9))
+        self.assertEqual(23, algo.optimal(end_inclusive=9))
+
+    def test_end_on_neither(self):
+        algo = MultiplesThreeFive()
+        self.assertEqual(33, algo.brute_force(end_inclusive=11))
+        self.assertEqual(33, algo.optimal(end_inclusive=11))
+
+    def test_actual_euler_question(self):
+        algo = MultiplesThreeFive()
+        self.assertEqual(233168, algo.brute_force(end_inclusive=999))
+        self.assertEqual(233168, algo.optimal(end_inclusive=999))
+
+
+if __name__ == "__main__":
+    unittest.main()
